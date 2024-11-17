01665f42715029b2a7de5df5dec40b4653328b0d
Kanishk Rai <kru.infosec254@gmail.com>
2024-11-17 18:49:59 +0530

    model for community

---
+++ back/models/community.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const communitySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Community = mongoose.model('Community', communitySchema);
+
+module.exports = Community;
